67c6f47fb133e3926384adf65fc9b676d050fef5
DESKTOP-FBH0P1R\34605 <camposvmiguelangel@gmail.com>
2021-05-14 14:48:12 +0200

    modal window view & functionality

--- script.js
+++ script.js
@@ -1,63 +1,97 @@
-let playerColumn = document.querySelector('#player-column')
-let pcImgs = Array.from(document.querySelectorAll('.pc-img'))
 let playerImgs = Array.from(document.querySelectorAll('.player-img'))
+let pcImgs = Array.from(document.querySelectorAll('.pc-img'))
 let playerPoints = 0
 let pcPoints = 0
 let playerChoice
 let pcChoice 
-let interval
-let finished = true
+let gameOnCourse = false
 
-playerImgs.forEach(playerImg)=>{
-    playerImg.onclick = (e) => {
-    if(finished===true){
-        finished = false
-        clearBorder()
-        pcPlay()
-        e.target.classList.add('pcPlay')
-        pcPlayEffect()
-        player = e.target.alt
-        setTimeout(() => {
-            finished = true
-            compareResult()
-        }, 2400);
-    }
+let playerName = document.querySelector("#name")
+let enterButton = document.querySelector("#enter-button")
+let gameModeOptions = Array.from(document.querySelectorAll("input[type='radio']"))
+let gameMode 
+
+
+
+enterButton.onclick= () =>{
+    setGameMode()
+    if(playerName.value!="" && gameMode!=undefined){
+        playerName.innerHTML= playerName.value
+        document.querySelector("#game").classList.toggle("hidden")
+        document.querySelector("#modal").classList.toggle("hidden")   
+    }else alert("Please complete the name and select the game mode.")
 }
+
+function setGameMode(){
+    gameModeOptions.forEach(gameModeOption =>
+        {if(gameModeOption.checked) gameMode=gameModeOption.id})
 }
-function pcPlay() {
-    num = Math.ceil(Math.random() * 3)
-    if (num == 1)
-    pcChoice = "rock"
-    else if (num == 2)
-    pcChoice = "papper"
-    else
-    pcchoice = "scissors"
-    return pcChoice
+function playerButtonsProgram(){
+    playerImgs.forEach(playerImg => {
+        playerImg.onclick = (e) => {
+            if(!gameOnCourse){
+                gameOnCourse = true
+                setStyles(e.target)
+                setPcChoice()
+                playerChoice = e.target.alt
+                pcPlayEffect()
+                setTimeout(() => {
+                    compareResult()
+                    gameOnCourse = false
+                }, 2400);
+            }
+        }
+    })
+}
+
+function setStyles(img) {
+    pcImgs.forEach( pcImg => pcImg.classList.remove("pcPlay"))
+    playerImgs.forEach( playerImg => playerImg.classList.remove("pcPlay"))
+    img.classList.add("pcPlay")
+}
+
+function setPcChoice() {
+    let num = Math.ceil(Math.random() * 3)
+    if (num === 1) pcChoice = "rock"
+    else if (num === 2) pcChoice = "papper"
+    else if(num === 3) pcChoice = "scissors" 
 }
 
 function compareResult() {
-    if (pcChoice == playerchoice)
+    if (pcChoice == playerChoice)
         showResultMessage("Tie")
-    else if ((player == "rock" && pc == "papper") || (player == "papper" && pc == "scissors") || (player == "scissors" && pc == "rock")) {
-        let pcScore = document.querySelector('#pc-score')
-        pcPoints++
-        pcScore.innerHTML = pcPoints
+    else if ((playerChoice == "rock" && pcChoice == "papper") || (playerChoice == "papper" && pcChoice == "scissors") || (playerChoice == "scissors" && pcChoice == "rock")) {
         showResultMessage("You lose...")
+        setTimeout(() => {
+            let pcScore = document.querySelector('#pc-score')
+            pcPoints++
+            pcScore.innerHTML = pcPoints
+            if(pcPoints==gameMode) setTimeout(()=> endGame(),1000) 
+        }, 1200);
     }
     else {
-        let playerScore = document.querySelector('#player-score')
-        playerPoints++
-        playerScore.innerHTML = playerPoints
         showResultMessage("You win!")
+        setTimeout(() => {
+            let playerScore = document.querySelector('#player-score')
+            playerPoints++
+            playerScore.innerHTML = playerPoints
+            if(playerPoints==gameMode) setTimeout(()=> endGame(),1000) 
+        }, 1200);
     }
+}
+function endGame(){
+    document.querySelector("#game").classList.toggle("hidden")
+    document.querySelector("#modal-finished").classList.toggle("hidden")
+
 }
 
 function showResultMessage(message) {
     let resultMessage = document.querySelector('#result-message')
     resultMessage.innerHTML = message
-    setTimeout(() => { resultMessage.innerHTML = ""; }, 2000)
+    setTimeout(() => { resultMessage.innerHTML = ""; }, 1500)
 }
 
+let interval
 function pcPlayEffect() {
     let counter = 0;
     startEffect();
@@ -65,11 +99,11 @@ function pcPlayEffect() {
         counter++;
         if (counter >= 3) {
             stopEffect();
-            if (pc === "rock") {
+            if (pcChoice === "rock") {
                 stayOn(0);
-            } else if (pc === "papper") {
+            } else if (pcChoice === "papper") {
                 stayOn(1);
-            } else if (pc === "scissors") {
+            } else if (pcChoice === "scissors") {
                 stayOn(2);
             }
         }
@@ -88,23 +122,19 @@ function startEffect() {
 }
 
 function addRemoveBorder(img) {
-    pcImg[img].classList.toggle("pcPlay")
-    setTimeout(function () { pcImg[img].classList.toggle("pcPlay") }, 150);
+    pcImgs[img].classList.toggle("pcPlay")
+    setTimeout(function () { pcImgs[img].classList.toggle("pcPlay") }, 150);
 }
 
 function stayOn(img) {
-    setTimeout(function () { pcImg[img].classList.add("pcPlay") }, 450);
-}
-
-function clearBorder() {
-    pcImg.forEach(pcImg)=> pcImg.classLisst.remove("pcPlay")
-    playerColumn.children.forEach(child)=> child.remove("pcPlay")
+    setTimeout(function () { pcImgs[img].classList.add("pcPlay") }, 470);
 }
 
 function stopEffect() {
     clearInterval(interval)
 }
 
+playerButtonsProgram()
 
 
 
